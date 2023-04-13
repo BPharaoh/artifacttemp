8bfdf5854093cc9a77701568c22fdbccaccfa3db
BPharaoh <94399798+BPharaoh@users.noreply.github.com>
2023-04-13 01:36:25 +0100

    matchtwoimport

---
+++ Artifact/Assets/Puppeteer/TwoOfAKind/Scripts/TOKPairsImage.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+namespace TwoOfAKindGame
+{
+    /// <summary>
+    /// This script displays a list of text pairs, each pair contains a First Text and a Second Text
+    /// </summary>
+    public class TOKPairsImage : MonoBehaviour
+    {
+        public Sprite[] pairsImage;
+
+        void Awake()
+        {
+            // If we have a game controller, assign the list of text pairs to it
+            if (GetComponent<TOKGameController>()) GetComponent<TOKGameController>().pairsImage = pairsImage;
+        }
+	}
+}
---
+++ Artifact/Assets/Puppeteer/TwoOfAKind/Scripts/TOKPairsText.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+namespace TwoOfAKindGame
+{
+    /// <summary>
+    /// This script displays a list of images ( Sprite )
+    /// </summary>
+    public class TOKPairsText : MonoBehaviour
+    {
+        public string[] pairsText;
+
+        void Awake()
+        {
+            // If we have a game controller, assign the list of images to it
+            if (GetComponent<TOKGameController>()) GetComponent<TOKGameController>().pairsText = pairsText;
+        }
+	}
+}
