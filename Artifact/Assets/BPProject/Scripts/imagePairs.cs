b068429352503a3d36ea661fa2df3ba47750f45d
BPharaoh <94399798+BPharaoh@users.noreply.github.com>
2023-04-13 03:59:57 +0100

    fixed it?

---
+++ Artifact/Assets/BPProject/Scripts/imagePairs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+namespace Project
+{
+    /// <summary>
+    /// This script displays a list of text pairs, each pair contains a First Text and a Second Text
+    /// </summary>
+    public class imagePairs : MonoBehaviour
+    {
+        public Sprite[] pairsImage;
+
+        void Awake()
+        {
+            // If we have a game controller, assign the list of text pairs to it
+            if (GetComponent<gameController>()) GetComponent<gameController>().pairsImage = pairsImage;
+        }
+	}
+}
